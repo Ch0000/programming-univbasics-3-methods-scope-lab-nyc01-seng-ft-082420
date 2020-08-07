9812766c381271cec2a2869c0c95717e32eadc85
Christian P Andrews <cpandrews91@gmail.com>
2020-08-07 03:49:12 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,26 +1,23 @@
-
-def all_phrases(mario, toadstool, link)
-  puts mario
-  puts toadstool
-  puts link
-end
-
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
-mario
+mario(phrase)
 
 def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
-toadstool
+toadstool(status)
 
 def link
   opening = "It's Dangerous To Go Alone! Take This."
   puts opening
 end
-link
+link(opening)
 
-all_phrases.new
+def all_phrases 
+  mario
+  toadstool
+  link
+end
